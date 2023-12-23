c0446f01cfc4fe10f69f2a6eb19ad4fb7448d908
JacobJS7 <j.szczur@interia.pl>
2023-12-23 14:57:51 +0100

    Wallet history restyled. Operations color has been added

--- src/views/UserPanel/Wallet/Operations.tsx
+++ src/views/UserPanel/Wallet/Operations.tsx
@@ -3,30 +3,31 @@ import styles from "./styles";
 import { Text } from "react-native";
 const Operations = () => {
 	const operationsData = [
-		{ date: "23-11-2023", cost: "23.64zł" },
-		{ date: "22-11-2023", cost: "21.72zł" },
-		{ date: "21-11-2023", cost: "12.14zł" },
-		{ date: "20-11-2023", cost: "23.64zł" },
-		{ date: "19-11-2023", cost: "21.72zł" },
-		{ date: "18-11-2023", cost: "12.14zł" },
-		{ date: "17-11-2023", cost: "23.64zł" },
-		{ date: "16-11-2023", cost: "21.72zł" },
-		{ date: "15-11-2023", cost: "12.14zł" },
-		{ date: "14-10-2023", cost: "23.64zł" },
-		{ date: "13-10-2023", cost: "21.72zł" },
-		{ date: "12-10-2023", cost: "12.14zł" },
-		{ date: "11-10-2023", cost: "23.64zł" },
-		{ date: "10-10-2023", cost: "21.72zł" },
-		{ date: "09-10-2023", cost: "12.14zł" },
-		{ date: "08-10-2023", cost: "23.64zł" },
-		{ date: "07-10-2023", cost: "21.72zł" },
+		{ date: "23-11-2023", cost: "23.64zł", type: "inflow" },
+		{ date: "22-11-2023", cost: "21.72zł", type: "inflow" },
+		{ date: "21-11-2023", cost: "12.14zł", type: "outflow" },
+		{ date: "20-11-2023", cost: "23.64zł", type: "inflow" },
+		{ date: "19-11-2023", cost: "21.72zł", type: "outflow" },
+		{ date: "18-11-2023", cost: "12.14zł", type: "outflow" },
+		{ date: "17-11-2023", cost: "23.64zł", type: "outflow" },
+		{ date: "16-11-2023", cost: "21.72zł", type: "inflow" },
+		{ date: "15-11-2023", cost: "12.14zł", type: "inflow" },
+		{ date: "14-10-2023", cost: "23.64zł", type: "outflow" },
+		{ date: "13-10-2023", cost: "21.72zł", type: "inflow" },
+		{ date: "12-10-2023", cost: "12.14zł", type: "outflow" },
+		{ date: "11-10-2023", cost: "23.64zł", type: "inflow" },
+		{ date: "10-10-2023", cost: "21.72zł", type: "outflow" },
+		{ date: "09-10-2023", cost: "12.14zł", type: "inflow" },
+		{ date: "08-10-2023", cost: "23.64zł", type: "inflow" },
+		{ date: "07-10-2023", cost: "21.72zł", type: "outflow" },
 	];
 	return (
 		<>
 			{operationsData.map((operation, index) => (
-				<Text key={index} style={styles.singleOperationText}>
-				Przejazd: {operation.date} - Koszt: {operation.cost}
-			  </Text>
+			<Text key={index} style={styles.singleOperationText}>
+				<Text style={{fontSize: 13}}> Przejazd: {operation.date} </Text>   
+				<Text style={{color: operation.type === 'inflow' ? 'green' : 'red'}}>   Koszt: {operation.cost}</Text>
+			</Text>
 			))}
 		</>
 	);
